8f38708c307c4e46591a3f2827c0191610c8a676
Ibra-Alpha BA <ibbraba@gmail.com>
2024-02-24 15:07:00 +0100

    Creating models

---
+++ Models/Equipment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppEnergy.Models
+{
+    abstract class Equipment
+    {
+        public int Id { get; set; } 
+
+        public int IdClient { get; set; }
+
+        public DateTime Date {  get; set; }
+
+        public string Status { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppEnergy.Models
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public string LastName { get; set; }
+        public string Login { get; set; }    
+        public string Password { get; set; }
+
+        public string Role { get; set; }
+
+
+
+    }
+}
